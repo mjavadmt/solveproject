f745b969afbfac56682652d5829f9211ae91bacb
mjavadmt <mjavadmt80@gmail.com>
2020-09-21 13:21:14 +0330

    newone

--- djangoProject/djangoapp/admin.py
+++ djangoProject/djangoapp/admin.py
@@ -1,7 +1,38 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-admin.site.register(Temp)
+# Register your models here.
+admin.site.register(Employer)
+admin.site.register(Freelancer)
+
+
+class employerInline(admin.StackedInline):
+    model = Employer
+    can_delete = False
+    verbose_name_plural = 'epmployer'
+
+
+# Define a new User admin
+
+
+
+class freelancerrInline(admin.StackedInline):
+    model = Freelancer
+    can_delete = False
+    verbose_name_plural = 'freelancer'
+
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (employerInline,freelancerrInline)
+
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(Project)
 
 
 # Register your models here.
--- djangoProject/djangoapp/models.py
+++ djangoProject/djangoapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -9,3 +10,28 @@ class Temp(models.Model):
         return f"{self.pk} {self.message}"
 
 
+class Employer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Freelancer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Project(models.Model):
+    producer = models.ForeignKey(Employer, related_name="projects_produced", on_delete=models.CASCADE, blank=True)
+    solver = models.ForeignKey(Freelancer, related_name="projects_solved", on_delete=models.CASCADE, blank=True)
+    interested_freelancers = models.ManyToManyField(Freelancer, related_name="interested_projects", blank=True)
+    price_employer = models.IntegerField(blank=True)
+
+
+class Price_Suggested_By_Freelancer(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    freelancer = models.ForeignKey(Freelancer, on_delete=models.CASCADE)
+    price = models.IntegerField(blank=True)
+
+# class Client(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     mobile_number = models.IntegerField(blank=True)
+#     # interested_projects = models.ForeignKey(Project, related_name="interested_freelancer")
+#     # projects = models.ForeignKey(Project , on_delete=models.CASCADE)
