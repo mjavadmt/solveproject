85523405d10a9687b0c74ee863bce73b61213e0a
ali alamdari <ali.alamdari9088@gmail.com>
2020-10-29 18:32:06 +0330

    jalal

--- djangoProject/djangoapp/models.py
+++ djangoProject/djangoapp/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime
-from django_jalali.db import models as jmodels
 
 
 # Create your models here.
